488bdb40c2f5d0949288f0fed6ad544e41dd73e5
soultampered <42450113+soultampered@users.noreply.github.com>
2024-01-08 23:37:21 -0500

    header button styles
    refactoring button styles

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,12 @@ import React from "react";
 const Header = () => {
     return (
         <header>
-            <div className="headerContainer">
+            <div className="inline">
                 <div className="logo">
                     <img src="/resources/images/VbT_Logo.svg" alt="siteLogo"/>
                 </div>
                 <nav>
-                    <ul className="flex">
+                    <ul className="flex justify-end">
                         <li className="headerBtn"><a href="@/components/Header#about">About</a></li>
                         <li className="headerBtn"><a href="@/components/Header#demos">Demos</a></li>
                         <li className="headerBtn"><a href="@/components/Header#clients">Clients</a></li>
