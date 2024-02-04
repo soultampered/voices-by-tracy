1f97db1f8a709bd37de2ca82b51b924c5f98ec6f
soultampered <42450113+soultampered@users.noreply.github.com>
2024-02-03 23:13:26 -0500

    Update Header.js
    added cursor styles

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,10 @@ const Header = () => {
                         <img src="/resources/images/VbT_Logo.svg" alt="siteLogo" className="h-32" />
                     </a>
                     <nav className="md:ml-auto md:mr-4 md:py-1 md:pl-4 flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900">About</a>
-                        <a className="mr-5 hover:text-gray-900">Demos</a>
-                        <a className="mr-5 hover:text-gray-900">Clients</a>
-                        <a className="mr-5 hover:text-gray-900">Contact</a>
+                        <a className="mr-5 hover:text-gray-900 cursor-pointer">About</a>
+                        <a className="mr-5 hover:text-gray-900 cursor-pointer">Demos</a>
+                        <a className="mr-5 hover:text-gray-900 cursor-pointer">Clients</a>
+                        <a className="mr-5 hover:text-gray-900 cursor-pointer">Contact</a>
                     </nav>
                     <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
                         Français
