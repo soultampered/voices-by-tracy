70290e44add1c2c4f381f8a83ff5b2bc523a5757
soultampered <42450113+soultampered@users.noreply.github.com>
2024-03-21 23:15:35 -0400

    Client Grid
    Set fixed height

--- components/ClientGrid.js
+++ components/ClientGrid.js
@@ -4,7 +4,7 @@ import ClientListCard from '@components/ClientListCard.js';
 
 const ClientGrid = () => {
     return (
-        <div className='w-2/3 bg-green-700 rounded ml-2 box-border inline-block h-auto p-2'>
+        <div className='w-2/3 bg-green-700 rounded ml-2 box-border inline-block h-720 p-2'>
             <h3>Clients Portfolio</h3>
             <ClientListCard />
 
