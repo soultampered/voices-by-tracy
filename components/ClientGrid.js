b97fb7d4e3ca209dfa2ff0b7c966d5bcc87a8f37
soultampered <42450113+soultampered@users.noreply.github.com>
2024-03-21 23:09:12 -0400

    Client Grid
    Added client portfolio grid

--- components/ClientGrid.js
+++ components/ClientGrid.js
@@ -1,10 +1,19 @@
 "use client"
 import React from "react";
+import ClientListCard from '@components/ClientListCard.js';
 
 const ClientGrid = () => {
     return (
-        <div className='w-2/3 bg-green-700 rounded ml-2 box-border h-720'>
+        <div className='w-2/3 bg-green-700 rounded ml-2 box-border inline-block h-auto p-2'>
+            <h3>Clients Portfolio</h3>
+            <ClientListCard />
 
+            <div className='w-full bg-purple-700 mt-2 rounded box-border'>
+                <p className='text-xl text-center'>This is some text This is some text This is some text This is some text This is some text</p>
+                <p className='text-lg text-center'>This is some text This is some text This is some text This is some text</p>
+                <p className='text-base text-center'>This is some text This is some text This is some text</p>
+                <p className='text-sm text-center'>This is some text</p>
+            </div>
         </div>
     );
 }
---
+++ components/ClientListCard.js
@@ -0,0 +1,100 @@
+"use client"
+import React from "react";
+
+const ClientListCard = () => {
+    return (
+        <div className='grid grid-cols-4 grid-rows-2 grid-flow-row gap-2'>
+
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-end px-4 pt-4">
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/resources/images/leithBust.jpg"
+                         alt="Bonnie image"/>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-end px-4 pt-4">
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/resources/images/leithBust.jpg"
+                         alt="Bonnie image"/>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-end px-4 pt-4">
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/resources/images/leithBust.jpg"
+                         alt="Bonnie image"/>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-end px-4 pt-4">
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/resources/images/leithBust.jpg"
+                         alt="Bonnie image"/>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-end px-4 pt-4">
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/resources/images/leithBust.jpg"
+                         alt="Bonnie image"/>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-end px-4 pt-4">
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/resources/images/leithBust.jpg"
+                         alt="Bonnie image"/>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-end px-4 pt-4">
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/resources/images/leithBust.jpg"
+                         alt="Bonnie image"/>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+            </div>
+
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-end px-4 pt-4">
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/resources/images/leithBust.jpg"
+                         alt="Bonnie image"/>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default ClientListCard;
