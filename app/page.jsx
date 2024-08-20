dc48ff246d0f4002a58a3bf18e76dcb8496eb8b7
soultampered <42450113+soultampered@users.noreply.github.com>
2024-08-20 14:00:04 -0400

    Disable Site Entry Point
    Site is still in development.  Disabled CTA button for entry

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,25 @@
 'use client';
-
 export default function LandingPage() {
-
     return (
-            <div className="bg-white h-screen flex items-center justify-center">
-                <div className="m-auto">
-                    <div>
-                        <div className="relative inline-block">
-                            <img src="/resources/images/VbT_Logo.svg"  alt="landing page"
-                                className="w-full h-auto"
-                                style={{
-                                    width: 1520,
-                                    height: 1059
-                                }}/>
-                            <div className="absolute inset-0 flex items-center justify-center"></div>
-                            <div className="relative inset-0 flex items-center justify-center">
-                                {/*<a href="/site" className="blueBtn w-1/4 h-20 text-5xl flex items-center justify-center">*/}
-                                {/*    Enter*/}
-                                {/*</a>*/}
-                            </div>
+        <div className="bg-white h-screen flex items-center justify-center">
+            <div className="m-auto">
+                <div>
+                    <div className="relative inline-block">
+                        <img src="/resources/images/VbT_Logo.svg"  alt="landing page"
+                            className="w-full h-auto"
+                            style={{
+                                width: 1520,
+                                height: 1059
+                            }}/>
+                        <div className="absolute inset-0 flex items-center justify-center"></div>
+                        <div className="relative inset-0 flex items-center justify-center">
+                            {/*<a href="/site" className="blueBtn w-1/4 h-20 text-5xl flex items-center justify-center">*/}
+                            {/*    Enter*/}
+                            {/*</a>*/}
                         </div>
                     </div>
                 </div>
-
             </div>
+        </div>
     );
 }
