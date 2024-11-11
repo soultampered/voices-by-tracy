78ac1f58c63b675cd9620857a84e955bcf9f6f45
soultampered <42450113+soultampered@users.noreply.github.com>
2024-11-11 11:08:36 -0500

    taiga-10: adjust landing page
    Gave enter button room for different aspect ratios

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -14,17 +14,12 @@ export default async function LandingPage({ params: { locale }}) {
                 <div className="absolute top-2 right-4 lg:top-4"><LanguageChanger /></div>
                 <div className="m-auto">
                     <div>
-                        <div className="bg-gradient rounded-3xl relative shadow-2xl p-4 border-2 w-[90vw] h-[90vh] max-w-[1080px] max-h-[720px] flex flex-col items-center justify-center">
+                        <div className="bg-gradient rounded-3xl relative shadow-2xl p-4 border-2 landscape-width w-[90vw] h-[90vh] max-w-[1080px] max-h-[720px] flex flex-col items-center justify-center">
                             <img src="/resources/images/VbT_Logo.svg"
                                  alt="landing page"
-                                 className="w-full h-auto"
-                                 style={{
-                                     maxWidth: '80%',
-                                     height: 'auto',
-                                 }}
-                            />
+                                 className="w-full max-w-[80%] max-h-[80%] object-contain"/>
                             <a href="/site"
-                               className="blueBtn w-3/4 md:w-1/4 h-20 text-5xl flex items-center justify-center">
+                               className="blueBtn mt-52 sm:mt-0 inline-flex items-center justify-center px-4 py-2 text-xl sm:text-3xl">
                                 {t("enterButton")}
                             </a>
                         </div>
