03b66d9148f9459984dcb04c9a59d8adf0a1219f
soultampered <42450113+soultampered@users.noreply.github.com>
2025-01-24 17:32:17 -0500

    Taiga-33
    Improved site performance for landing page.

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -2,6 +2,7 @@ import initTranslations from "../i18n";
 import TranslationsProvider from "@components/TranslationsProvider";
 import LanguageChanger from "@components/LanguageChanger";
 import React from "react";
+import Image from "next/image";
 
 const i18nNamespaces = ['common','buttons']
 
@@ -14,14 +15,19 @@ export default async function LandingPage({ params: { locale }}) {
                 <div className="absolute top-2 right-4 lg:top-4"><LanguageChanger /></div>
                 <div className="m-auto">
                     <div>
-                        <div className="landing-page-background rounded-3xl relative shadow-2xl p-4 border-2 landscape-width w-[90vw] h-[90vh] max-w-[1080px] max-h-[720px] flex flex-col items-center justify-center">
-                            <img src="/resources/images/VbT_Logo.svg"
-                                 alt="landing page"
-                                 className="w-full max-w-[80%] max-h-[80%] object-contain"/>
-                            <a href="/site"
-                               className="blueBtn mt-52 sm:mt-0 inline-flex items-center justify-center px-4 py-2 text-xl sm:text-3xl">
-                                {t("buttons:enterButton")}
-                            </a>
+                        <div className="landing-page-background rounded-3xl relative shadow-2xl p-4 border-2 landscape-width w-[90vw] max-w-[1080px] max-h-[720px] flex flex-col items-center justify-center">
+                            <div className="w-full max-w-[80%] max-h-[80%] flex flex-col items-center">
+                                <Image src="/resources/images/VbT_Logo.svg"
+                                   alt="landing page"
+                                   className="object-contain"
+                                   layout="responsive"
+                                   width={750}
+                                   height={550}/>
+                                <a href="/site"
+                                   className="blueBtn mt-52 sm:mt-0 px-4 py-2 text-xl sm:text-3xl">
+                                   {t("buttons:enterButton")}
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
