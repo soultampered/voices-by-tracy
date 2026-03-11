5a375473d5c870c7f646e57982dc3359096d5283
soultampered <42450113+soultampered@users.noreply.github.com>
2026-03-11 01:07:45 -0400

    configure cloudinary sdk

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,13 @@
 const nodeEnv = process.env.NODE_ENV || "development";
 
-const requiredServerEnvVars = ["MONGODB_URI", "EMAIL", "EMAIL_PASS"];
+const requiredServerEnvVars = [
+    "MONGODB_URI",
+    "EMAIL",
+    "EMAIL_PASS",
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
+];
 
 const missingServerEnvVars = requiredServerEnvVars.filter((key) => !process.env[key]);
 
@@ -18,4 +25,7 @@ export const environment = {
     email: process.env.EMAIL,
     emailPass: process.env.EMAIL_PASS,
     i18nexusApiKey: process.env.I18NEXUS_API_KEY || "",
+    cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 };
